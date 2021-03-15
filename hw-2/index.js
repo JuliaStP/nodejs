6e6b64820d44e6eab832d44bc71e44e05b859762
Julia Andreeva <andreeva_julia@hotmail.com>
2021-03-15 09:59:10 -0400

    modified hw-2

--- hw-2/index.js
+++ hw-2/index.js
@@ -4,7 +4,7 @@ const timeout = process.env.TM || 1000;
 const interval = process.env.INT || 100;
 
 http.createServer((req, res) => {
-  if (req.url === '/') {
+  if (req.url) {
     console.log('Started Time: ' + timeout + ', Interval:' + interval);
 
     let timerOn = true;
@@ -23,5 +23,7 @@ http.createServer((req, res) => {
         res.end('<h1>' + Date() + '</h1>');
       }
     }, interval);
+  } else {
+    console.error("There was an error");
   }
-}).listen(5500);
+}).listen(5500)
