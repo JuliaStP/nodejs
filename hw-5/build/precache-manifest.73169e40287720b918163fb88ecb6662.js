419ebb5738bd893966bb3dabd25d8a91df526b00
Julia Andreeva <andreeva_julia@hotmail.com>
2021-04-06 09:45:12 -0400

    build

---
+++ hw-5/build/precache-manifest.73169e40287720b918163fb88ecb6662.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93268b042aeeb7659602148119297649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62045fcfc61fce4fdf2f",
+    "url": "/static/js/2.9e8b5b8b.chunk.js"
+  },
+  {
+    "revision": "3ec87150ff597f4ee867",
+    "url": "/static/js/main.7e66b684.chunk.js"
+  },
+  {
+    "revision": "32996fb276b3e890a01d",
+    "url": "/static/js/runtime~main.01574ed8.js"
+  }
+]);
