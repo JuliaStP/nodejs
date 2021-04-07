8eb998e6eb880bd0ddbf90f177104e10e6b6c443
Julia Andreeva <andreeva_julia@hotmail.com>
2021-04-07 10:28:26 -0400

    modified server/index.js

--- server/index.js
+++ server/index.js
@@ -1,29 +1,6 @@
 const http = require('http')
 const express = require('./express')
 const server = http.createServer(express) 
-const { Pool } = require('mongoose');
-const router = express.Router();
-
-const pool = new Pool({
-  DATABASE_URL: 'mongodb+srv://loft-systems:10LoftSchool@cluster0.fji0e.mongodb.net/loft-systems?retryWrites=true&w=majority',
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-router.get('/database', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/database', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
 
 require('./socket/socket')(server) 
 require('./database/connect');
