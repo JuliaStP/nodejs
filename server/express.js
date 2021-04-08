687d7875f309c4ea0728fa3de9432cd9801e94cc
Julia Andreeva <andreeva_julia@hotmail.com>
2021-04-08 07:17:22 -0400

    finished project

--- server/express.js
+++ server/express.js
@@ -12,7 +12,7 @@ app.use('/api', require('./routes'))
 
 app.use('*', (_req, res) => {
   const file = path.resolve(__dirname, '../build', 'index.html')
-  res. sendFile(file)
+  res. sendFile(file) //check
 })
 
 app.use((err, _, res, __) => {
